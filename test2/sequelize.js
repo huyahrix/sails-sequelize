a1dcd0155c5032ff0b32e5e4e914101aef844951
Huy Phạm <infjnite@gmail.com>
2020-12-01 17:36:27 +0700

    test

---
+++ test2/sequelize.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var models = require('./models')
+
+models.sequelize.sync().then(function () {
+  models.Customer.findAndCountAll().then(function (result) {
+    console.dir(result)
+    console.log(result.count)
+    console.log(result.rows)
+  })
+})
